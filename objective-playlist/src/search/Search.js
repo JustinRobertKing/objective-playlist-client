a98a6ff7a0685075e0dd61cf0eb8b6a392a19e3c
JustinRobertKing <kingj4@spu.edu>
2019-06-07 16:40:44 -0700

    made a successful api call to spotify with oauth

--- objective-playlist/src/search/Search.js
+++ objective-playlist/src/search/Search.js
@@ -11,22 +11,41 @@ import axios from 'axios'
 
 class Search extends React.Component {
   state = {
-    search: ''
+    query: ''
   }
 
   handleChange = (e) => {
-    this.setState({ search: e.target.value });
+    this.setState({ query: e.target.value });
   };
 
   handleSubmit = (e) => {
+    console.log('this.state', this.state);
     e.preventDefault()
-    axios.get
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=track';
+    var accessToken = 'BQDT6PWvr06AMPXiUkYQSWJbQSBrvPIWqEGAA_IIzNGUSPI0GiYqEST7cP7y29pPAVIMf3LFwrLQzpqNhkyJV8hJqXONI3mScUCR8kbHRXnxBeC7fbYMpObsKW2xvzZ_HkylKapoOMgWhVa8P5L22o92i7_nzOhQ_smx&refresh_token=AQCQeahbPpN2m2EjR-Ex5-ck3v1O6RaAprs7RMDTlh1KH9m2mW21lRrhbGjAJ-P7jsLIcvDrgVyabvDGNPdOXHawk7xVF7lJk7qcfwPD00dF0MamDPVGjbAYRT5dmxyu8VwllA'
+
+    var myOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    fetch(FETCH_URL, myOptions)
+      .then(response => response.json())
+      .then(json => {
+        console.log('duhjson: ', json)
+        this.setState({ json });
+      })
   }
 
   render() {
     return (
       <div>
-        <form className="container" noValidate autoComplete="off">
+        <form className="container" noValidate autoComplete="off" onSubmit={this.handleSubmit}>
           <TextField
             id="filled-search"
             label="Search by song"
@@ -36,6 +55,7 @@ class Search extends React.Component {
             type="search"
             margin="normal"
             variant="filled"
+            onChange={this.handleChange}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
