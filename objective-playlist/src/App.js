01ba4ce2a77ecce8cbee021200c98ddff8f42a22
JustinRobertKing <kingj4@spu.edu>
2019-06-07 14:20:12 -0700

    created search component; added axios

---
+++ objective-playlist/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ye from './spotify.png'
+import './App.css';
+import Search from './search/Search'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ye} style={{width: 200, height: 'auto'}} />
+        
+      </header>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ objective-playlist/src/search/Search.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import axios from 'axios'
+
+class Search extends React.Component {
+  state = {
+    search: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="container" noValidate autoComplete="off">
+          <TextField
+            id="filled-search"
+            label="Search by song"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            type="search"
+            margin="normal"
+            variant="filled"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                  >
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
